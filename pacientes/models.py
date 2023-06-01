907d674d556cb39d552cfcf946e2f562e08f1d25
davidvaldh <ds.valderrama@uniandes.edu.co>
2023-06-01 11:27:55 -0500

    Data model finished and proof of first migration

--- pacientes/models.py
+++ pacientes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Paciente(models.Model):
+    id = models.AutoField(primary_key=True)
+    idCard = models.IntegerField() 
+    name = models.CharField(max_length=50)
+    sex = models.CharField(max_length=1, choices=(('M', 'Masculino'), ('F', 'Femenino'), ('O', 'Otro')))
+    email= models.EmailField(max_length=254)
+    phone = models.IntegerField()
+    address = models.CharField(max_length=50)
